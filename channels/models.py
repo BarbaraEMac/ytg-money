4c85ba5512832c42352c75a2f19f7120a39cc10a
Barbara Macdonald <z4beth@gmail.com>
2017-05-20 18:10:08 -0700

    Added channels model

---
+++ channels/models.py
@@ -0,0 +1,69 @@
+import httplib2
+import json
+import logging
+import os
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+from oauth2client.contrib.appengine import CredentialsNDBProperty
+from apiclient.discovery import build
+
+foo = os.path.dirname(__file__) + "/client_secrets.json"
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account.
+YOUTUBE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+class Channel(ndb.Model):
+    # Added this object was created
+    created_date = ndb.DateTimeProperty(auto_now_add=True)
+
+    # Oauth crednetials for auth'ing http requests
+    credentials = CredentialsNDBProperty()
+
+    # Json for aOauth crednetials for auth'ing http requests
+    credentials_json = ndb.JsonProperty()
+
+    # google's unique identifier for the Users service
+    user_id = ndb.StringProperty()
+
+    # YouTube channel id
+    external_id = ndb.StringProperty(default="", indexed=True)
+
+    # Channel's display name
+    name = ndb.StringProperty(default="", indexed=False)
+
+    # Email address
+    email = ndb.StringProperty()
+
+    @classmethod
+    def _pre_put_hook(self):
+        logging.info("Saving a new Channel: %s" % self.external_id)
+
+    @classmethod
+    def _pre_get_hook(self):
+        logging.info("Fetching Channel: %s" % self.external_id)
+
+    @staticmethod
+    def get_or_create( credentials, user ):
+
+        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    http=credentials.authorize(httplib2.Http()))
+
+        channel_details = youtube.channels().list(mine=True, part='id, snippet').execute()
+        external_id = channel_details['items'][0]['id']
+        name = channel_details['items'][0]['snippet']['title']
+
+        channel = Channel.query(Channel.external_id == external_id).get()
+
+        if channel is None:
+            channel = Channel(credentials = credentials,
+                              credentials_json = credentials.to_json(),
+                              external_id = external_id,
+                              name = name,
+                              user_id = user.user_id(),
+                              email = user.email())
+            channel.put()
+
+        return channel
