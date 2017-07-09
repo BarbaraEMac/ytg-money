6a5adb379b6d42b88f3ad468cf13dcc6d29f09a4
Barbara Macdonald <z4beth@gmail.com>
2017-07-08 20:39:13 -0700

    Added Super Chat fetcher

---
+++ superchats/models.py
@@ -0,0 +1,109 @@
+import constants
+import httplib2
+import json
+import logging
+import os
+
+import helpers
+
+from alerts.models import Alert
+from viewers.models import Viewer
+
+from datetime import datetime
+from google.appengine.ext import ndb
+from apiclient.discovery import build
+
+# https://developers.google.com/youtube/v3/live/docs/superChatEvents
+
+class SuperChat(ndb.Model):
+    created_date = ndb.DateTimeProperty(auto_now_add=True)
+    created_at  = ndb.DateTimeProperty()
+
+    id = ndb.StringProperty(indexed=True)
+
+    owner_channel_id = ndb.StringProperty()
+
+    viewer = ndb.StructuredProperty(Viewer)
+
+    amount = ndb.FloatProperty()
+
+    currency = ndb.StringProperty()
+
+    usd_amount = ndb.FloatProperty()
+
+    displayString = ndb.StringProperty() # Pretty print for orignial amount+currency
+
+    tier = ndb.IntegerProperty()
+
+    text  = ndb.TextProperty()
+
+    @staticmethod
+    def get_superchats( credentials, requesting_channel_id ):
+
+        youtube = helpers.auth_http( credentials )
+
+        request = youtube.superChatEvents().list( part="id,snippet",
+                                           maxResults=50 )
+
+        while request:
+            # Fetch first batch of 50
+            response = request.execute()
+            superchats = response["items"]
+
+            logging.info("GETSUPERCHATS: %i" % len(superchats))
+            if len(superchats) == 0:
+                return
+
+            for superchat_obj in superchats:
+                id = superchat_obj["id"]
+                superchat = superchat_obj["snippet"]
+
+                owner_channel_id = superchat['channelId']
+
+                channel_id = superchat['supporterDetails']['channelId']
+                channel_url = superchat['supporterDetails']['channelUrl']
+                name = superchat['supporterDetails']['displayName']
+                image = superchat['supporterDetails']['profileImageUrl']
+
+                text = superchat['commentText']
+                created_at = datetime.strptime( superchat["createdAt"], "%Y-%m-%dT%H:%M:%S.%fZ" )
+                amount = int( superchat['amountMicros'] )
+                currency = superchat['currency']
+                displayString = superchat['displayString']
+                tier = superchat['messageType']
+
+                sc = SuperChat.query( SuperChat.id == id ).get()
+
+                # If we've already saved this one, keep going
+                if sc:
+                    continue
+
+                # Otherwise, get the viewer corresponding to this user
+                viewer = Viewer.query( Viewer.channel_id == channel_id ).get()
+
+                # Make a vew Viewer if we don't have one
+                if viewer is None:
+                    viewer = Viewer( channel_id = channel_id,
+                                     channel_url = channel_url,
+                                     name = name,
+                                     image = image
+                                   )
+                viewer.put()
+
+                # Now, make the new superchat and save it
+                sc = SuperChat( owner_channel_id = owner_channel_id,
+                                viewer           = viewer,
+                                id = id,
+                                text = text,
+                                created_at = created_at,
+                                amount = amount,
+                                currency = currency,
+                                displayString = displayString,
+                                tier = tier )
+                sc.put()
+
+                Alert.create_superchat_alert( viewer, text, amount )
+                # end for
+
+            request = youtube.superChatEvents().list_next( request, response )
+            # end while
---
+++ superchats/worker.py
@@ -0,0 +1,45 @@
+import constants
+import logging
+import time
+import webapp2
+
+from channels.models import Channel
+from constants       import is_barbara_live, BARBARA_CHANNEL_ID
+from superchats.models import SuperChat
+from videos.models   import Video
+
+from google.appengine.api import taskqueue
+
+class EnqueueSuperChatsFetchTaskHandler (webapp2.RequestHandler):
+
+    def get(self):
+
+        task = taskqueue.add( queue_name = "superchats-queue",
+                              url = "/superchats/fetcher"
+                            )
+        self.response.out.write('Task {} enqueued, ETA {}.'.format(task.name, task.eta))
+
+class SuperChatsFetcherHandler( webapp2.RequestHandler ):
+
+    def post(self):
+        logging.info ("Starting to look for superchats")
+
+        channel_creds= Channel.get_barbara_creds()
+
+        if channel_creds is None:
+            logging.info("Sponsor Fetcher: No channel")
+            return
+
+        logging.info("Checking for new superchats")
+        SuperChat.get_superchats( channel_creds, BARBARA_CHANNEL_ID )
+
+        if is_barbara_live():
+            #time.sleep(5)
+            task = taskqueue.add( queue_name = "superchats-queue",
+                                  url = "/superchats/fetcher"
+                                )
+        logging.info("No longer looking for new superchats")
+
+app = webapp2.WSGIApplication([ ('/superchats/fetcher', SuperChatsFetcherHandler),
+                                ('/superchats/enqueue', EnqueueSuperChatsFetchTaskHandler)
+                              ], debug=True)
